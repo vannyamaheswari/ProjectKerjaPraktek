db42fa14a8d27d3550d95db58ded59f9641f5796
maulisalzt <46183892+maulisalzt@users.noreply.github.com>
2020-02-27 16:10:44 +0700

    added

--- edustage/platinum_person.php
+++ edustage/platinum_person.php
@@ -98,7 +98,7 @@ hr {
                       >
                     </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="contact.html">Hubungi Kami</a>
+                  <a class="nav-link" href="menu_daftar.html">Register</a>
                 </li>
                 
               </ul>
